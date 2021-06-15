8f06c91245f6630463c555693ca8d3bebc0398b8
Nasim Uddin <23162353+thenasim@users.noreply.github.com>
2021-06-15 18:43:31 +0600

    jsconfig

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import Head from "next/head";
-import { DarkModeButton } from "../components/DarkModeButton";
+import { DarkModeButton } from "@/components/DarkModeButton";
 
 export default function Home() {
   return (
