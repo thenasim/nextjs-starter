487df980d784ec23a07dfb3535c548e441b97fcb
Nasim Uddin <23162353+thenasim@users.noreply.github.com>
2021-07-12 22:54:36 +0600

    tailwind color added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,29 @@
+function withOpacity(variableName) {
+  return ({ opacityValue }) => {
+    if (opacityValue !== undefined) {
+      return `hsla(var(${variableName}), ${opacityValue})`;
+    }
+    return `hsl(var(${variableName}))`;
+  };
+}
+
 module.exports = {
   mode: "jit",
   purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: "class", // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      // ? Text
+      textColor: {},
+
+      // ? Background
+      backgroundColor: {
+        body: {
+          light: withOpacity("--bg-body-light"),
+          dark: withOpacity("--bg-body-dark"),
+        },
+      },
+    },
   },
   variants: {
     extend: {},
