9fe8fcc6702cf6abefdc702534be67c23f771d57
Nasim Uddin <23162353+thenasim@users.noreply.github.com>
2021-09-17 16:17:50 +0600

    Fixed: dark mode system theme issue

--- components/DarkModeButton.tsx
+++ components/DarkModeButton.tsx
@@ -6,18 +6,27 @@ import { BsMoon } from "react-icons/bs";
 import { FiSun } from "react-icons/fi";
 
 export const DarkModeButton = () => {
-  const { theme, setTheme } = useTheme();
+  const { theme, systemTheme, setTheme } = useTheme();
   const [mounted, setMounted] = useState(false);
 
+  const activeTheme = theme === "system" ? systemTheme : theme;
+
   useEffect(() => setMounted(true), []);
+
+  const toggleTheme = () => {
+    if (activeTheme === "light") {
+      return setTheme("dark");
+    }
+    return setTheme("light");
+  };
 
   return (
     <button
       aria-label="Toggle dark mode button"
-      onClick={() => (theme === "light" ? setTheme("dark") : setTheme("light"))}
+      onClick={toggleTheme}
       className="px-3 py-3 text-base font-semibold text-gray-800 bg-gray-100 rounded-md focus:outline-none dark:bg-gray-800 dark:text-gray-100"
     >
-      {mounted ? theme === "dark" ? <FiSun /> : <BsMoon /> : <BsMoon />}
+      {mounted ? activeTheme === "dark" ? <FiSun /> : <BsMoon /> : <BsMoon />}
     </button>
   );
 };
