cf6189bf8195348a59e271cec4c77dabd5b572c2
Nasim Uddin <23162353+thenasim@users.noreply.github.com>
2021-10-28 23:01:09 +0600

    default theming added in css variables

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,4 @@
+const colors = require("tailwindcss/colors");
 const { fontFamily } = require("tailwindcss/defaultTheme");
 
 function withOpacity(variableName) {
@@ -16,17 +17,56 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
     "./layouts/**/*.{js,ts,jsx,tsx}",
   ],
-  darkMode: "class", // or 'media' or 'class'
+  darkMode: "class",
   theme: {
     fontFamily: {
       sans: [...fontFamily.sans],
     },
-    extend: {
-      colors: {
-        body: withOpacity("--body"),
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      white: withOpacity("--color-white"),
+      black: withOpacity("--color-black"),
+
+      gray: {
+        50: withOpacity("--color-gray-50"),
+        100: withOpacity("--color-gray-100"),
+        200: withOpacity("--color-gray-200"),
+        300: withOpacity("--color-gray-300"),
+        400: withOpacity("--color-gray-400"),
+        500: withOpacity("--color-gray-500"),
+        600: withOpacity("--color-gray-600"),
+        700: withOpacity("--color-gray-700"),
+        800: withOpacity("--color-gray-800"),
+        900: withOpacity("--color-gray-900"),
       },
-      textColor: {},
-      backgroundColor: {},
+
+      body: {
+        primary: withOpacity("--body-primary"),
+        "primary-2": withOpacity("--body-primary-2"),
+        "primary-3": withOpacity("--body-primary-3"),
+
+        "primary-inverse": withOpacity("--body-primary-inverse"),
+        "primary-inverse-2": withOpacity("--body-primary-inverse-2"),
+        "primary-inverse-3": withOpacity("--body-primary-inverse-3"),
+      },
+
+      txt: {
+        primary: withOpacity("--txt-primary"),
+        "primary-2": withOpacity("--txt-primary-2"),
+        "primary-3": withOpacity("--txt-primary-3"),
+      },
+
+      // Tailwind colors
+      blue: colors.blue,
+      cyan: colors.cyan,
+      sky: colors.sky,
+      indigo: colors.indigo,
+      violet: colors.violet,
+      purple: colors.purple,
+      red: colors.red,
+      orange: colors.orange,
+      yellow: colors.yellow,
     },
   },
   variants: {
