90121471ff09c71028f13077cf0074b4faec911f
Nasim Uddin <23162353+thenasim@users.noreply.github.com>
2022-02-25 02:26:32 +0600

    Jest setup

--- components/DarkModeButton.tsx
+++ components/DarkModeButton.tsx
@@ -9,7 +9,7 @@ export const DarkModeButton = () => {
     <button
       aria-label="Toggle dark mode button"
       onClick={toggleTheme}
-      className="px-3 py-3 text-base font-semibold text-gray-800 bg-gray-100 rounded-md focus:outline-none dark:bg-gray-800 dark:text-gray-100 focus-within:ring-2 dark:focus-within:ring-gray-700 focus-within:ring-gray-200 ring-offset-1 ring-offset-body-main"
+      className="px-3 py-3 text-base font-semibold text-gray-800 bg-gray-100 rounded-md focus:outline-none dark:bg-gray-800 dark:text-gray-100 focus-visible:ring-2 dark:focus-visible:ring-gray-700 focus-visible:ring-gray-200 ring-offset-1 ring-offset-body-main"
     >
       {isDark ? <BiSun size="1.3em" /> : <BiMoon size="1.3em" />}
     </button>
